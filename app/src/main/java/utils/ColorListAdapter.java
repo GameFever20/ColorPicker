3c7aac5fb3f36d411f10765822fe661b6f369fd9
Aisha <priyank20patel@gmail.com>
2017-07-24 03:08:10 +0530

    almost completed

--- app/src/main/java/utils/ColorListAdapter.java
+++ app/src/main/java/utils/ColorListAdapter.java
@@ -1,12 +1,15 @@
 package utils;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,11 +40,18 @@ public class ColorListAdapter extends RecyclerView.Adapter<ColorListAdapter.Colo
     @Override
     public void onBindViewHolder(ColorListAdapter.ColorListAdapterHolder holder, int position) {
 
-        final Detail detail = colorListArrayList.get(position);
-        holder.mPrimaryColorTextView.setText(detail.getPrimaryHexCode());
-        holder.mPrimaryDarkColorTextView.setText(detail.getPrimaryDarkHexCode());
-        holder.mAccentColorTextView.setText(detail.getAccentHexCode());
+        try {
+            final Detail detail = colorListArrayList.get(position);
+            holder.mPrimaryColorTextView.setText(detail.getPrimaryHexCode());
+            // holder.mPrimaryDarkColorTextView.setText(detail.getPrimaryDarkHexCode());
+            holder.mAccentColorTextView.setText(detail.getAccentHexCode());
 
+            holder.mPrimaryBackground.setBackgroundColor(Color.parseColor(detail.getPrimaryHexCode()));
+            holder.mAccentBackground.setCardBackgroundColor(Color.parseColor(detail.getAccentHexCode()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -51,16 +61,20 @@ public class ColorListAdapter extends RecyclerView.Adapter<ColorListAdapter.Colo
     }
 
     public class ColorListAdapterHolder extends RecyclerView.ViewHolder {
-        public TextView mPrimaryColorTextView, mPrimaryDarkColorTextView, mAccentColorTextView, mShowColorTextView;
+        public TextView mPrimaryColorTextView, mPrimaryDarkColorTextView, mAccentColorTextView;
+        public CardView mPrimaryBackground;
+        public CardView mAccentBackground;
 
 
         public ColorListAdapterHolder(View view) {
             super(view);
             mPrimaryColorTextView = (TextView) view.findViewById(R.id.colorlist_adapter_header_primarycolor_textview);
-            mPrimaryDarkColorTextView = (TextView) view.findViewById(R.id.colorlist_adapter_header_primarydarkcolor_textview);
             mAccentColorTextView = (TextView) view.findViewById(R.id.colorlist_adapter_header_accentcolor_textview);
-         //
-            //   mShowColorTextView = (TextView) view.findViewById(R.id.colorlist_adapter_header_showColor_textview);
+
+            // mPrimaryDarkColorTextView = (TextView) view.findViewById(R.id.colorlist_adapter_header_primarydarkcolor_textview);
+            mPrimaryBackground = (CardView) view.findViewById(R.id.colorlist_adapter_primarycolor_cardview);
+            mAccentBackground = (CardView) view.findViewById(R.id.colorlist_adapter_accentcolor_fab_cardview);
+
 
         }
     }
